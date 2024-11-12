415539e46daa042b81fc8d1afe1b9901a32faaee
Daniel <dwgrossberg@gmail.com>
2024-11-12 12:41:19 -0500

    Cleanup POST methods

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, redirect, url_for, request
+from flask import Flask, jsonify, request
 from flask_cors import CORS, cross_origin
 from flask.helpers import send_from_directory
 import csv
@@ -17,38 +17,30 @@ CORS(app)
 
 @app.route('/authentication', methods=['POST'])
 @cross_origin()
-def authenticate():
-    data = request.get_json()  # {IP address, is_human? <bool>}
+def authenticate(IP):
+    data = request.get_json()  # {IP address}
     with open(data.csv, 'r') as csv_file:
         reader = csv.reader(csv_file)
         for row in reader:
-            if data in row:
-                return redirect(url_for('human'))
-    return redirect(url_for('not_human'))
+            if IP in row:
+                # IP already on file
+                return jsonify({'message': 'Human authenticated'})
+    # IP must be authenticated
+    return jsonify({'message': 'Not authenticated'})
 
 
 @app.route('/whitelist', methods=['POST'])
 @cross_origin()
-def whitelist():
+def whitelist(IP):
     data = request.get_json()  # {IP address, is_human? <bool>}
+    # check for is_human identifier
+    if not data['is_human']:
+        return jsonify({'message': 'Not authenticated'})
+    # save IP for future checks
     with open(data.csv, 'w') as csv_file:
         writer = csv.writer(csv_file)
-        writer.writerow([data])
-    return redirect(url_for('human'))
-
-
-@app.route('/not_human', methods=['GET'])
-@cross_origin()
-def not_human():
-    data = 'Not human'
-    return jsonify(data)
-
-
-@app.route('/human', methods=['GET'])
-@cross_origin()
-def human():
-    data = 'Verified human'
-    return jsonify(data)
+        writer.writerow([IP])
+    return jsonify({'message': 'Human authenticated'})
 
 
 @app.route('/')
